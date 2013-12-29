1c917bdb56c4214e09f147b5321dfb011d56183c
h8rift <h8rift@gmail.com>
2013-12-29 13:47:36 -0500

    msm: HTC: m4: force chipid 3050 for Adreno for using new Adreno and KGSL
    Change-Id: I7fa1810b5f98742117ee60bb8d51cf5c67324427

--- arch/arm/mach-msm/htc/m4ul/board-m4-gpu.c
+++ arch/arm/mach-msm/htc/m4ul/board-m4-gpu.c
@@ -166,8 +166,8 @@ static struct platform_device device_kgsl_3d0 = {
 
 void __init m4_init_gpu(void)
 {
-	unsigned int version = socinfo_get_version();
-
+/*	unsigned int version = socinfo_get_version();
+*/
 	/* Set the turbo speed for the AA and AB respectively */
 
 	if (cpu_is_msm8930aa())
@@ -179,13 +179,15 @@ void __init m4_init_gpu(void)
 
 	/* Set up the chip ID based on the SoC version */
 
-	if (cpu_is_msm8930ab())
+/*	if (cpu_is_msm8930ab())
 		kgsl_3d0_pdata.chipid = ADRENO_CHIPID(3, 0, 5, 3);
 	else if ((SOCINFO_VERSION_MAJOR(version) == 1) &&
 		(SOCINFO_VERSION_MINOR(version) == 2))
 		kgsl_3d0_pdata.chipid = ADRENO_CHIPID(3, 0, 5, 2);
 	else
 		kgsl_3d0_pdata.chipid = ADRENO_CHIPID(3, 0, 5, 0);
+*/
+	kgsl_3d0_pdata.chipid = ADRENO_CHIPID(3, 0, 5, 0);
 
 	platform_device_register(&device_kgsl_3d0);
 }
