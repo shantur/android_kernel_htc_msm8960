21bd907633d2bec6feaa3d829710109870154bc1
Flemmard <flemmard@gmail.com>
2014-02-07 21:22:36 +0100

    pyramid: update for AAT

--- arch/arm/mach-msm/htc/pyramid/board-pyramid.c
+++ arch/arm/mach-msm/htc/pyramid/board-pyramid.c
@@ -579,7 +579,7 @@ static struct platform_device htc_battery_pdev = {
 #endif
 
 
-#ifdef CONFIG_FLASHLIGHT_AAT1271
+#ifdef CONFIG_FLASHLIGHT_AAT
 static void config_flashlight_gpios(void)
 {
 	static uint32_t flashlight_gpio_table[] = {
@@ -602,7 +602,7 @@ static struct flashlight_platform_data flashlight_data = {
 };
 
 static struct platform_device flashlight_device = {
-	.name = "FLASHLIGHT_AAT1271",
+	.name = AAT_FLT_DEV_NAME,
 	.dev = {
 		.platform_data	= &flashlight_data,
 	},
@@ -1546,7 +1546,7 @@ static struct platform_device *early_devices[] __initdata = {
 #endif
 	&msm_device_dmov_adm0,
 	&msm_device_dmov_adm1,
-#ifdef CONFIG_FLASHLIGHT_AAT1271
+#ifdef CONFIG_FLASHLIGHT_AAT
 	&flashlight_device,
 #endif
 };
